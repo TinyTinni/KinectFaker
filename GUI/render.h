8abe6196d7084b27c359d6b00779ce1e7bb57bf7
Matthias Möller <m_moeller@live.de>
2017-07-25 12:43:30 +0200

    remove dx11 from GUI as requirement (everything is based on OpenGL (v3) now)

--- GUI/render.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <d3d11.h>
-
-void Alloc_Ressources(ID3D11Device* device, ID3D11DeviceContext* context);
-bool Render(ID3D11Device* device, ID3D11DeviceContext* context);
-void Finish();
